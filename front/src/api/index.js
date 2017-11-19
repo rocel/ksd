708e664e48ee5f5f9c69b36a2c197fce8ed06749
damien <damien.chaillou@gmail.com>
2017-11-19 10:46:57 -0200

    UI enhancement and store information fix

--- front/src/api/index.js
+++ front/src/api/index.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
 export default axios.create({
-  baseURL: '/api', // baseURL: 'http://localhost:3000/'
+  baseURL: 'api', // baseURL: 'http://localhost:3000/'
   timeout: 5000
 })
--- src/main/java/com/rocel/ksd/IWebService.java
+++ src/main/java/com/rocel/ksd/IWebService.java
@@ -3,7 +3,7 @@ package com.rocel.ksd;
 import org.apache.kafka.streams.KafkaStreams;
 
 public interface IWebService {
-    void start(KafkaStreams streams, String host, int port);
+    void start(KafkaStreams streams, int port, String zkKhost);
 
     void stop();
 }
